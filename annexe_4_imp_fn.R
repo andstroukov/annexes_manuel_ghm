1d515067e68de67926400614d58ed57337ff5864
Strukov Andrey <andrey.strukov@aphp.fr>
2024-06-05 16:30:54 +0200

    Annexe 4, années 2023 et 2024 fonctionnel

--- annexe_4_imp_fn.R
+++ annexe_4_imp_fn.R
@@ -3,13 +3,10 @@
 
 library(tidyverse)
 library(pdftools)
-library(tictoc)
 # 
 # Extraction des données brutes du fichier PDF
 
 # sous forme de large character ####
-#
-tic()
 # 
 an<-2023
 #
@@ -21,15 +18,14 @@ fn_reperage_pages<-function(num_page){
   pg<-extra_pdf[[num_page]]
   pg%>%
     select(y,text)%>%
-    filter(str_detect(text,"4-"),y>790)%>%
+    filter(str_detect(text,"4-"),y>770)%>%
     mutate(num=if_else(!is.na(text),num_page,NA))%>%
     filter(!is.na(num))%>%
     select(num)
 }
 # 
+pg<-extra_pdf[[308]]
 pages<-map_df(1:pdf_info(chemin_pdf)$pages,fn_reperage_pages)
-pages<-pages%>%
-  distinct()
 #
 # Extraction Annexe 4 de diag/liste exclu ####
 # selon position x
@@ -39,7 +35,7 @@ fn_extr_tab_pages <- function(num_page){
     filter(x>73,x<86)%>%
     select(y,cim=text)%>%
     left_join(.,extra_pdf[[num_page]]%>%
-                filter(x>156,x<169)%>%
+                filter(x>142,x<169)%>%
                 select(y,liste_exclu=text))%>%
     select(cim,liste_exclu)
 }
@@ -52,6 +48,6 @@ tbl <- map_df(min(pages):max(pages),fn_extr_tab_pages)%>%
 # Export tableau Annexe 4 ###
 #
 write.csv2(tbl,file=paste0("tb1_annexe_4_",an,".csv"),row.names = F)
-toc()
+
 
 
